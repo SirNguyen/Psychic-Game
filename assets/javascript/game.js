188c455ab255ad8f0d7d87f20386a218d93445f3
Tim Nguyen <nguyentimvan@gmail.comgitconfig --global user.name Tim>
2018-10-08 21:32:36 -0700

    Adjusted the index.html and game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,23 @@
-var alphabet = ["a", 
-                "b", 
-                "c", 
-                "d", 
-                "e", 
-                "f", 
-                "g", 
-                "h", 
-                "i", 
-                "j", 
-                "k", 
-                "l", 
-                "m", 
-                "n", 
-                "o", 
-                "p", 
-                "q", 
-                "r", 
-                "s", 
-                "t", 
-                "u", 
-                "v", 
-                "x", 
-                "y", 
-"z"];
+alert("You have 9 guesses!");
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesSofar = [];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+
+var computerGuess = alphabet[(Math.floor(Math.random() *26))];
+
+function scoreboard(){
+    
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "losses: " + losses;
+    document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+}
+
+var reset = function() {
+    totalGuesses = 9;
+    guesses = 9;
+    guessedLetters = [];
+    }
+
